ba71dcc09912c3ae9acecebadb8d90863fe949a1
Ahmedhady <ahmed.abdelhady.saleh@gmail.com>
2024-04-06 08:38:49 +0200

    1

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,33 @@
-export default function Page() {
+//from etsh
+/*export default function Page() {
     return <p>Customers Page</p>;
+  }*/
+
+  import Search from '@/app/ui/search';
+  import Table from '@/app/ui/customers/table';
+  import { lusitana } from '@/app/ui/fonts';
+  import { Suspense } from 'react';
+  import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+  import { Metadata } from 'next';
+  
+  export default async function Page({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+      page?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+  
+    return (
+      <div className="w-full">
+        <Table customers={customers} />
+      </div>
+    );
   }
+  
+  export const metadata: Metadata = {
+    title: 'Customers',
+  };
